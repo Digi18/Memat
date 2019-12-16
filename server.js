0314433672e856ed4007394c04a349f1b58b3f46
Digi18 <me.digvijay18@gmail.com>
2019-12-17 00:23:17 +0530

    new route added

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.use(require('./routes/getPersonalized.js'));
-app.use(require('./routes/getGeneral.js'));
+app.use(require('./routes/getPersonal.js'));
+app.use(require('./routes/personal.js'));
 
 const port = process.env.PORT || 3000;
 
